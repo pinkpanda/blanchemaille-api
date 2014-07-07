279253dd9057491652e31510d3990eaa4964b7dc
Baptiste Lecocq <contact@tiste.io>
2014-07-07 18:08:57 +0100

    Structure app and add rabl

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'sinatra', '1.4.5'
+gem 'sinatra',  '1.4.5'
+
+gem 'mysql2'
+gem 'rabl',     '0.10.1'
+gem 'sinatra-activerecord'
+gem 'sinatra-reloader'
 gem 'unicorn'
--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
-require 'sinatra/base'
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require 'rabl'
 
-class API < Sinatra::Base
-  get '/' do
-  end
+require './models.rb'
+
+
+config = YAML.load(File.read('config/database.yml'))
+RACK_ENV ||= ENV['RACK_ENV'] || 'development'
+
+ActiveRecord::Base.establish_connection config[RACK_ENV]
+
+Rabl.configure do |config|
+  config.include_json_root  = false
+  config.include_child_root = false
+end
+
+
+before do
+  content_type 'application/json'
+end
+
+get '/' do
+  rabl :index, format: 'json'
 end
